3789569f53e558677386b44bda38966ea4db598c
xfxftop1gg <xfxf2099@gmail.com>
2026-02-08 14:25:59 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Конфигурация Supabase - ЗАМЕНИТЕ НА СВОИ ДАННЫЕ
-const SUPABASE_URL = 'https://ваш-проект.supabase.co';
-const SUPABASE_ANON_KEY = 'ваш-anon-ключ';
+const SUPABASE_URL = 'https://nnpzazihtzbqmopnayxp.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ucHphemlodHpicW1vcG5heXhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1MTcyNzUsImV4cCI6MjA4NjA5MzI3NX0.iU4nlXfrNoLxTbPrKsiAKZU0vq-iAl5BFGtY-Mk9R14';
 
 // Инициализация клиента Supabase
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
@@ -22,4 +22,5 @@ supabaseClient.auth.getSession().then(({ data, error }) => {
     } else {
         console.log('Supabase: Подключение успешно установлено');
     }
+
 });
